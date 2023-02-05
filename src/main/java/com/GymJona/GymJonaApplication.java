0d719371accc6f89894e2d746e0f549b80667571
JONA <JONA@Jona-2022>
2023-02-04 22:20:58 -0600

    Tarea practica 2 página web

---
+++ src/main/java/com/GymJona/GymJonaApplication.java
@@ -0,0 +1,13 @@
+package com.GymJona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymJonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymJonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GymJona/GymJonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GymJona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymJonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
